88291222e9d953708814a1d7f67a972759147c3c
Francesco Paolo Lovergine <frankie@debian.org>
2025-10-03 12:13:04 +0200

    mInor changes to comments

--- build-iso.sh
+++ build-iso.sh
@@ -20,9 +20,14 @@ die() {
 # Write out the channels file so it can be included
 guix time-machine -C './guix/base-channels.scm' -- \
      describe -f channels > './guix/channels.scm' || exit 0
+#
+# Note that this outputs also specific commits to the channels file, which is not a great
+# idea in general...
+#
 
 # Build the image
 printf 'Attempting to build the image...\n\n'
+printf 'Note that this script also builds a full kernel, be sure to have at least 30GBs on /tmp or choose a different TMPDIR for guix-daemon\n\n'
 image=$(guix time-machine -C './guix/channels.scm' --substitute-urls='https://ci.guix.gnu.org https://bordeaux.guix.gnu.org https://substitutes.nonguix.org' -- system image -t iso9660 './guix/installer.scm') \
     || die 'Could not create image.'
 
